2ca161cb56259527fd14a4b9f14e070e852c19b3
Sajid Zaman <sajid-619@users.noreply.github.com>
2017-02-18 17:58:07 +0600

    Update languages.py

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -27,5 +27,14 @@ LANGUAGES = [
         ]
     },
     #  include the English language here
-    # HINT: https://en.wikipedia.org/wiki/Most_common_words_in_English
+     {
+        'name': 'English',
+        'common_words': [
+            'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I',
+            'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at',
+            'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she',
+            'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what',
+            'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me'
+        ]
+    }
 ]
